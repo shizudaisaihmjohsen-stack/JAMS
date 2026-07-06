89357738ca5fa1f8e30126eacbf7ab75f04faa6b
Shizudaisai Hm Johsen <shizudaisai.hm.johsen@gmail.com>
2026-07-06 18:14:58 +0900

    Avoid duplicate PWA window title

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "jams-shell-20260706-1";
+const CACHE_NAME = "jams-shell-20260706-2";
 const APP_SHELL = [
   "./",
   "./index.html",
--- pwa.js
+++ pwa.js
@@ -1,3 +1,10 @@
+const isStandaloneApp = window.matchMedia("(display-mode: standalone)").matches
+  || window.navigator.standalone === true;
+
+if (isStandaloneApp) {
+  document.title = "情報宣伝部 部員認証・管理システム";
+}
+
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("./service-worker.js", {
